5b24720df3b05a3e8cfe9e2bb08fe20cc942adc3
Espen Moe-Nilssen <espen@medialog.no>
2011-12-08 09:58:57 +0100

    added new viewlet , this is version b9

--- medialog/subskins/browser/viewlets.py
+++ medialog/subskins/browser/viewlets.py
@@ -3,3 +3,6 @@ from plone.app.layout.viewlets.common import ViewletBase
 
 class SubskinsLoader(ViewletBase):
     render = ViewPageTemplateFile('subskinsloader.pt')
+
+class SubskinsColophon(ViewletBase):
+    render = ViewPageTemplateFile('subskinscolophon.pt')

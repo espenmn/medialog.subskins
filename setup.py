fc08f906f334b43c05adcd5445a6807510ba5810
Espen Moe-Nilssen <espen@medialog.no>
2011-12-15 12:59:44 +0100

    typo in 15.css

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '4.2b10'
+version = '4.2b11'
 setup(name='medialog.subskins',
       version=version,
       description="An installable theme and theming tool for Plone 4",                

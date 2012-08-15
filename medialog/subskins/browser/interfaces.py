d95a5e36d781a9bda32b719fbecada608a2fbc34
Espen Moe-Nilssen <espen@medialog.no>
2012-08-15 09:45:30 +0200

    Added portlet manager in the footer

--- medialog/subskins/browser/interfaces.py
+++ medialog/subskins/browser/interfaces.py
@@ -1,5 +1,10 @@
 from plone.theme.interfaces import IDefaultPloneLayer
+from plone.portlets.interfaces import IPortletManager
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
+    """
+
+class IMedialogFooterContent(IPortletManager):
+    """we need our own portlet manager below the content area.
     """

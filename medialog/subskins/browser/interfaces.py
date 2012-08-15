37aff1811c4101351a3964e02a45c2a547729844
Espen Moe-Nilssen <espen@medialog.no>
2012-08-15 10:39:42 +0200

    a new try

--- medialog/subskins/browser/interfaces.py
+++ medialog/subskins/browser/interfaces.py
@@ -1,5 +1,6 @@
 from plone.theme.interfaces import IDefaultPloneLayer
 from plone.portlets.interfaces import IPortletManager
+from zope.viewlet.interfaces import IViewletManager
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
@@ -7,4 +8,8 @@ class IThemeSpecific(IDefaultPloneLayer):
 
 class IMedialogFooterContent(IPortletManager):
     """we need our own portlet manager below the content area.
+    """
+    
+class IAboveContentViews(IViewletManager):
+    """A viewlet manager that sits above the content views
     """

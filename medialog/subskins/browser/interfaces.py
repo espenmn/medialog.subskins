1ea39de13748a2fe0815e37347a73fda49eb42eb
Espen Moe-Nilssen <espen@medialog.no>
2012-08-16 10:35:29 +0200

    fixed interface so all portlets show (thanks to Dan Jacka)

--- medialog/subskins/browser/interfaces.py
+++ medialog/subskins/browser/interfaces.py
@@ -1,10 +1,10 @@
 from plone.theme.interfaces import IDefaultPloneLayer
-from plone.portlets.interfaces import IPortletManager
+from plone.app.portlets.interfaces import IColumn
 
 class IThemeSpecific(IDefaultPloneLayer):
     """Marker interface that defines a Zope 3 browser layer.
     """
 
-class IMedialogFooterContent(IPortletManager):
+class IMedialogFooterContent(IColumn):
     """we need our own portlet manager below the content area.
     """
